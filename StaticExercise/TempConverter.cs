45b1071c7925f81fb931badbf9ea2eb7bdbd151d
Jeffrey Baker <jeffrey1871@gmail.com>
2021-03-30 19:49:29 -0500

    Initial Commit

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f)
+        {
+            return (f - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double c)
+        {
+            return c * 1.8 + 32;
+        }
+    }
+}
